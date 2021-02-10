4ed372ca3ce195e589c0191a2723055f29f135e4
jmm <Jean-Marc.Molines@univ-grenoble-alpes.fr>
2021-02-10 21:08:17 +0100

    add BUILD directory to maintain scripts and record of the how to ...

---
+++ eORCA025/BUILD/DOMAIN_cfg/get_bat_coord.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# this script get the bathymetry and coordinates file form the MEOM opendap.
+# Those files are used in the preparation of the domain_cfg file
+
+URL=https://ige-meom-opendap.univ-grenoble-alpes.fr/thredds/fileServer/meomopendap/extract/eORCA025.L121/eORCA025.L121-I
+
+wget  --no-check-certificate $URL/eORCA025_bathymetry_b0.2.nc
+wget  --no-check-certificate $URL/eORCA025_coord_c3.0.nc 
+
+# make the link for make_domain_cfg.exe
+ln -sf eORCA025_bathymetry_b0.2.nc bathy_meter.nc
+ln -sf eORCA025_coord_c3.0.nc coordinates.nc
