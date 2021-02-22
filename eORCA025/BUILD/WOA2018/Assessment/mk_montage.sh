a86dc3cf7abcb46928240fb10f42a16d1836675a
jmm <Jean-Marc.Molines@univ-grenoble-alpes.fr>
2021-02-22 10:53:29 +0100

    modification in the mk_montage script to make it generic

--- eORCA025/BUILD/WOA2018/Assessment/mk_montage.sh
+++ eORCA025/BUILD/WOA2018/Assessment/mk_montage.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
-geometry="603x335"
+# script used to make a montage of several images in a single one
+# Note that in this script, it is assumed that thumbnails are organized 3 x 3 (hardcoded)
 
-PERIOD1=81B0
-PERIOD2=CLIM
-var=vosaline
-xtra=""
+geometry="603x335"     # pixel size of each thumbnail
+
+CONFIG=eORCA025.L75    # Nemo configuration on which data are interpolated
+DTASET=WOA18           # Name of the WOA data set used so far
+
+PERIOD1=81B0           # name of first WOA dataset
+PERIOD2=CLIM           # name of second WOA dataset
+var=vosaline           # netcdf variable in files
+xtra=""                # extra information such as depth, por instance '_1200'
 
 
 # 1 year
-montage -tile 1x3 -geometry $geometry eORCA025.L75_${PERIOD1}_WOA18_1y_${var}.png eORCA025.L75_${PERIOD2}_WOA18_1y_${var}.png eORCA025.L75_${PERIOD1}-${PERIOD2}_WOA18_1y_${var}.png WOA18_${var}${xtra}_1y_${PERIOD1}-${PERIOD2}_comp.png
+montage -tile 1x3 -geometry $geometry ${CONFIG}_${PERIOD1}_${DTASET}_1y_${var}.png ${CONFIG}_${PERIOD2}_${DTASET}_1y_${var}.png ${CONFIG}_${PERIOD1}-${PERIOD2}_${DTASET}_1y_${var}.png ${DTASET}_${var}${xtra}_1y_${PERIOD1}-${PERIOD2}_comp.png
 
 
 
@@ -16,48 +22,48 @@ for season in JFM AMJ JAS OND ; do
 cmd="montage -tile 3x3 -geometry $geometry "
 case $season in 
 (JFM) for m in {01..03} ; do
-         cmd="$cmd eORCA025.L75_${PERIOD1}_WOA18_1m_${var}_${m}.png "
+         cmd="$cmd ${CONFIG}_${PERIOD1}_${DTASET}_1m_${var}_${m}.png "
       done
       for m in {01..03} ; do
-         cmd="$cmd eORCA025.L75_${PERIOD2}_WOA18_1m_${var}_${m}.png "
+         cmd="$cmd ${CONFIG}_${PERIOD2}_${DTASET}_1m_${var}_${m}.png "
       done
       for m in {01..03} ; do
-         cmd="$cmd eORCA025.L75_${PERIOD1}-${PERIOD2}_WOA18_1m_${var}_${m}.png "
+         cmd="$cmd ${CONFIG}_${PERIOD1}-${PERIOD2}_${DTASET}_1m_${var}_${m}.png "
       done
-      cmd="$cmd WOA18_${var}${xtra}_${season}_${PERIOD1}-${PERIOD2}_comp.png"
+      cmd="$cmd ${DTASET}_${var}${xtra}_${season}_${PERIOD1}-${PERIOD2}_comp.png"
       $cmd ;;
 (AMJ) for m in {04..06} ; do
-         cmd="$cmd eORCA025.L75_${PERIOD1}_WOA18_1m_${var}_${m}.png "
+         cmd="$cmd ${CONFIG}_${PERIOD1}_${DTASET}_1m_${var}_${m}.png "
       done
       for m in {04..06} ; do
-         cmd="$cmd eORCA025.L75_${PERIOD2}_WOA18_1m_${var}_${m}.png "
+         cmd="$cmd ${CONFIG}_${PERIOD2}_${DTASET}_1m_${var}_${m}.png "
       done
       for m in {04..06} ; do
-         cmd="$cmd eORCA025.L75_${PERIOD1}-${PERIOD2}_WOA18_1m_${var}_${m}.png "
+         cmd="$cmd ${CONFIG}_${PERIOD1}-${PERIOD2}_${DTASET}_1m_${var}_${m}.png "
       done
-      cmd="$cmd WOA18_${var}${xtra}_${season}_${PERIOD1}-${PERIOD2}_comp.png"
+      cmd="$cmd ${DTASET}_${var}${xtra}_${season}_${PERIOD1}-${PERIOD2}_comp.png"
       $cmd ;;
 (JAS) for m in {07..09} ; do
-         cmd="$cmd eORCA025.L75_${PERIOD1}_WOA18_1m_${var}_${m}.png "
+         cmd="$cmd ${CONFIG}_${PERIOD1}_${DTASET}_1m_${var}_${m}.png "
       done
       for m in {07..09} ; do
-         cmd="$cmd eORCA025.L75_${PERIOD2}_WOA18_1m_${var}_${m}.png "
+         cmd="$cmd ${CONFIG}_${PERIOD2}_${DTASET}_1m_${var}_${m}.png "
       done
       for m in {07..09} ; do
-         cmd="$cmd eORCA025.L75_${PERIOD1}-${PERIOD2}_WOA18_1m_${var}_${m}.png "
+         cmd="$cmd ${CONFIG}_${PERIOD1}-${PERIOD2}_${DTASET}_1m_${var}_${m}.png "
       done
-      cmd="$cmd WOA18_${var}${xtra}_${season}_${PERIOD1}-${PERIOD2}_comp.png"
+      cmd="$cmd ${DTASET}_${var}${xtra}_${season}_${PERIOD1}-${PERIOD2}_comp.png"
       $cmd ;;
 (OND) for m in {10..12} ; do
-         cmd="$cmd eORCA025.L75_${PERIOD1}_WOA18_1m_${var}_${m}.png "
+         cmd="$cmd ${CONFIG}_${PERIOD1}_${DTASET}_1m_${var}_${m}.png "
       done
       for m in {10..12} ; do
-         cmd="$cmd eORCA025.L75_${PERIOD2}_WOA18_1m_${var}_${m}.png "
+         cmd="$cmd ${CONFIG}_${PERIOD2}_${DTASET}_1m_${var}_${m}.png "
       done
       for m in {10..12} ; do
-         cmd="$cmd eORCA025.L75_${PERIOD1}-${PERIOD2}_WOA18_1m_${var}_${m}.png "
+         cmd="$cmd ${CONFIG}_${PERIOD1}-${PERIOD2}_${DTASET}_1m_${var}_${m}.png "
       done
-      cmd="$cmd WOA18_${var}${xtra}_${season}_${PERIOD1}-${PERIOD2}_comp.png"
+      cmd="$cmd ${DTASET}_${var}${xtra}_${season}_${PERIOD1}-${PERIOD2}_comp.png"
       $cmd ;;
 esac
 
