667e78aef972a6f069a5fc8b25123528cece248b
jmm <Jean-Marc.Molines@univ-grenoble-alpes.fr>
2021-03-11 23:17:36 +0100

    add scripts for building the distance to coast file.
       * 3 flavours are given :
         - from original tmask
         - from modified tmask (islands supressed)
         - from runoff file

---
+++ eORCA025/BUILD/DISTCOAST/caldist_from_original_tmask.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# This script compute the distance to coast file from runoff file
+
+CF_MESH_HGR=eORCA025.L75_domain_cfg_closed_seas.nc
+CF_MESH_MASK=../eORCA025.L75_mesh_mask.nc
+CF_TMASK=$CF_MESH_MASK
+CF_3D_TFILE=eORCA025.L75_5564_WOA18_1y_vosaline.nc
+
+CF_DIST_COAST=eORCA025.L75_distcoast_v000.nc
+
+# Compute dist.coast file from the runoff file
+ cdfcofdis  -H $CF_MESH_HGR  -M $CF_TMASK  -T $CF_3D_TFILE  -surf  -nc4
+
+ # Medsea East + Black Sea + Red Sea
+ cdfbathy -f  dist.coast -v Tcoast -zoom 1156 1325 732 920   -sz 10000000
+ # Medsea West (Alboran sea)
+ cdfbathy -f dist.coast.01 -v Tcoast -zoom 1127 1168 831 873 -sz 10000000
+ # mask the resulting file (restoring islands, but not a pb)
+ cdfmltmask -f  dist.coast.02 -m $CF_MESH_MASK  -v Tcoast -p T
+ mv  dist.coast.02_masked $CF_DIST_COAST
+
+# extra cleaning of temporary files 
+rm dist.coast.01 dist.coast.02
+
