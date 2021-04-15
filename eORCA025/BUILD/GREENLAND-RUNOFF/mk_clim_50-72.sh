b622fffd03cce03743d0570fe818dd9141a23328
jmm <Jean-Marc.Molines@univ-grenoble-alpes.fr>
2021-04-15 19:49:54 +0200

    add mk_clim_50-72.sh

---
+++ eORCA025/BUILD/GREENLAND-RUNOFF/mk_clim_50-72.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+CONFIG=eORCA025.L75
+y1=1950
+y2=1972
+freq=1m
+
+cd  $WORK/${CONFIG}/${CONFIG}-I/GREENLAND-RUNOFF-build/ANNUAL
+
+for typ in calving isf rnf ; do
+  lst=''
+  for y in $( seq $y1 $y2 ) ; do
+   lst="$lst ${CONFIG}_${freq}_greenland_${typ}_y${y}.nc"
+  done
+  cdfmoyt -l $lst -nc4 -o  ${CONFIG}_${freq}_greenland_${typ}_clim_y${y1}-${y2}.nc
+done
+
+
